440625b7f478fed9141a8f5d94f2012b0f1c1f61
Luca Iacobini <luca.iacobini@mangrovia.io>
2025-09-25 10:53:30 +0200

    feat: Implement Button component and integrate across layouts

--- src/utils/size.ts
+++ src/utils/size.ts
@@ -1,5 +1,18 @@
 export type Size = "3xl" | "2xl" | "xl" | "lg" | "md" | "sm" | "xs";
 
+export function getButtonSize(size: Size = "md", primary = false) {
+  switch (size) {
+    case "md":
+      return "text-md py-3 " + (primary ? "px-5" : "px-4");
+    case "sm":
+      return "text-sm py-2 " + (primary ? "px-4" : "px-3");
+    case "xs":
+      return "text-xs py-2 " + (primary ? "px-4" : "px-3");
+    default:
+      return "";
+  }
+}
+
 export function getFractionSize(size: Size = "md") {
   switch (size) {
     case "3xl":
